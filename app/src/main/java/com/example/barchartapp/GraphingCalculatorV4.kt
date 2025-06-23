d8330d4fc41cb878d25fc5ca119f6bafd59ee8cb
TomiLakkakorpi <t9lato03@students.oamk.fi>
2025-06-23 18:07:02 +0300

    Updated circle plotting

--- app/src/main/java/com/example/barchartapp/GraphingCalculatorV4.kt
+++ app/src/main/java/com/example/barchartapp/GraphingCalculatorV4.kt
@@ -25,12 +25,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import co.yml.charts.axis.AxisData
 import co.yml.charts.common.extensions.formatToSinglePrecision
 import co.yml.charts.common.model.Point
+import co.yml.charts.ui.linechart.LineChart
 import co.yml.charts.ui.linechart.model.GridLines
+import co.yml.charts.ui.linechart.model.Line
+import co.yml.charts.ui.linechart.model.LineChartData
+import co.yml.charts.ui.linechart.model.LinePlotData
 import co.yml.charts.ui.linechart.model.LineStyle
 import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
 import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
@@ -46,6 +52,7 @@ import org.mariuszgromada.math.mxparser.Argument
 import org.mariuszgromada.math.mxparser.Expression
 
 var Calculator4lineChartList = mutableListOf<Point>()
+var Calculator4lineChartListCenter = mutableListOf<Point>()
 
 @Composable
 fun GraphingCalculatorScreen4(navController: NavController) {
@@ -54,23 +61,70 @@ fun GraphingCalculatorScreen4(navController: NavController) {
     var kText by remember { mutableStateOf("") }
     var rText by remember { mutableStateOf("") }
 
+    var hDisplayText = ""
+    var kDisplayText = ""
+    var rDisplayText = ""
+
     var h by remember { mutableFloatStateOf(0.0f) }
     var k by remember { mutableFloatStateOf(0.0f) }
     var r by remember { mutableFloatStateOf(0.0f) }
     var t by remember { mutableFloatStateOf(0.0f) }
 
-    var t1Value by remember { mutableFloatStateOf(0.0f) }
-    var t2Value by remember { mutableFloatStateOf(0.0f) }
-
     var e1: Expression
     var e2: Expression
-
-    var t2: Argument
 
     var xValue by remember {mutableFloatStateOf(0.0f)}
     var yValue by remember {mutableFloatStateOf(0.0f)}
 
-    var formula by remember {mutableStateOf("")}
+    var uiUpdate by remember {mutableStateOf("")}
+
+    hDisplayText = if(hText == "") {
+        "-h"
+    } else if(hText == "-") {
+        "-h"
+    } else if(hText.toFloat() == 0.0f) {
+        "-0"
+    } else {
+        if(hText.toFloat() > 0.0f) {
+            "+$hText"
+        } else if (hText.toFloat() < 0.0f){
+            hText
+        } else {
+            "-h"
+        }
+    }
+
+    kDisplayText = if(kText == "") {
+        "-k"
+    } else if(kText == "-") {
+        "-k"
+    } else if(kText.toFloat() == 0.0f) {
+        "-0"
+    } else {
+        if(kText.toFloat() > 0.0f) {
+            "+$kText"
+        } else if (kText.toFloat() < 0.0f){
+            kText
+        } else {
+            "-k"
+        }
+    }
+
+    rDisplayText = if(rText == "") {
+        "r"
+    } else if(rText == "-") {
+        "r"
+    } else if(rText.toFloat() == 0.0f) {
+        "0"
+    } else {
+        if(rText.toFloat() > 0.0f) {
+            rText
+        } else if (rText.toFloat() < 0.0f){
+            "-$rText"
+        } else {
+            "-r"
+        }
+    }
 
     Box(
         modifier = Modifier
@@ -90,12 +144,12 @@ fun GraphingCalculatorScreen4(navController: NavController) {
 
             val xAxisData = AxisData.Builder()
                 .axisStepSize(30.dp)
-                .startDrawPadding(48.dp)
+                .startDrawPadding(5.dp)
                 .steps(Calculator4lineChartList.size - 1)
                 .labelAndAxisLinePadding(25.dp)
                 .labelData { i ->
-                    val xMin = Calculator4lineChartList.minOf { it.x }
-                    val xMax = Calculator4lineChartList.maxOf { it.x }
+                    val xMin = Calculator4lineChartList.minOf { it.x } //* 1.25f
+                    val xMax = Calculator4lineChartList.maxOf { it.x } //* 1.25f
                     val xScale = (xMax - xMin) / steps
                     ((i * xScale) + xMin).formatToSinglePrecision()
                 }.build()
@@ -104,20 +158,28 @@ fun GraphingCalculatorScreen4(navController: NavController) {
                 .steps(steps)
                 .labelAndAxisLinePadding(20.dp)
                 .labelData { i ->
-                    val yMin = Calculator4lineChartList.minOf { it.y }
-                    val yMax = Calculator4lineChartList.maxOf { it.y }
+                    val yMin = Calculator4lineChartList.minOf { it.y } //* 1.25f
+                    val yMax = Calculator4lineChartList.maxOf { it.y } //* 1.25f
                     val yScale = (yMax - yMin) / steps
                     ((i * yScale) + yMin).formatToSinglePrecision()
                 }.build()
 
-            val data = WaveChartData(
-                wavePlotData = WavePlotData(
+            val data = LineChartData(
+                linePlotData = LinePlotData(
                     lines = listOf(
-                        Wave(
+                        Line(
                             dataPoints = Calculator4lineChartList,
-                            waveStyle = LineStyle(color = Color.Black),
+                            lineStyle = LineStyle(color = Color.Black),
                             selectionHighlightPoint = SelectionHighlightPoint(),
-                            shadowUnderLine = ShadowUnderLine(),
+                            //shadowUnderLine = ShadowUnderLine(),
+                            selectionHighlightPopUp = SelectionHighlightPopUp()
+                            //waveFillColor = WaveFillColor(topColor = Color.Green, bottomColor = Color.Red),
+                        ),
+                        Line(
+                            dataPoints = Calculator4lineChartListCenter,
+                            lineStyle = LineStyle(color = Color.Black),
+                            selectionHighlightPoint = SelectionHighlightPoint(),
+                            //shadowUnderLine = ShadowUnderLine(),
                             selectionHighlightPopUp = SelectionHighlightPopUp()
                             //waveFillColor = WaveFillColor(topColor = Color.Green, bottomColor = Color.Red),
                         ),
@@ -130,7 +192,7 @@ fun GraphingCalculatorScreen4(navController: NavController) {
 
             Text(
                 modifier = Modifier.padding(10.dp),
-                text = "Piirretty Kaava: $formula"
+                text = "Piirretty kaava:"
             )
 
             Box(
@@ -142,57 +204,100 @@ fun GraphingCalculatorScreen4(navController: NavController) {
 
                 if(Calculator4lineChartList.isNotEmpty()){
                     Log.d("FormulaTest", "Drawing Chart")
-                    WaveChart(
+                    LineChart(
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(350.dp),
-                        waveChartData = data
+                        lineChartData = data
                     )
                 }
             }
 
-            TextField(
-                modifier = Modifier
-                    .padding(0.dp, 50.dp, 0.dp, 0.dp)
-                    .width(200.dp),
-                value = hText,
-                onValueChange = { newText ->
-                    hText = newText
-                },
-                label = {
-                    Text(text = "Syötä h")
-                },
-            )
+            Row() {
+                Text(
+                    text = "(x$hDisplayText)² - (y$kDisplayText)² = $rDisplayText²",
+                    fontSize = 25.sp
+                )
+            }
 
-            TextField(
-                modifier = Modifier.width(200.dp),
-                value = kText,
-                onValueChange = { newText ->
-                    kText = newText
-                },
-                label = {
-                    Text(text = "Syötä k")
-                },
-            )
+            Row() {
+                Text(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    text = "Syötä h: ",
+                    fontSize = 20.sp
+                )
 
-            TextField(
-                modifier = Modifier.width(200.dp),
-                value = rText,
-                onValueChange = { newText ->
-                    rText = newText
-                },
-                label = {
-                    Text(text = "Syötä r")
-                },
-            )
+                TextField(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .height(50.dp)
+                        .width(100.dp),
+                    value = hText,
+                    onValueChange = { newText ->
+                        hText = newText
+                    },
+                )
+            }
+
+            Row() {
+                Text(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    text = "Syötä k: ",
+                    fontSize = 20.sp
+                )
+
+                TextField(
+                    textStyle = TextStyle(fontSize = 15.sp),
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .height(50.dp)
+                        .width(100.dp),
+                    value = kText,
+                    onValueChange = { newText ->
+                        kText = newText
+                    }
+                )
+            }
+
+            Row() {
+                Text(
+                    modifier = Modifier
+                        .padding(10.dp),
+                    text = "Syötä r: ",
+                    fontSize = 20.sp
+                )
+
+                TextField(
+                    textStyle = TextStyle(fontSize = 15.sp),
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .height(50.dp)
+                        .width(100.dp),
+                    value = rText,
+                    onValueChange = { newText ->
+                        rText = newText
+                    }
+                )
+            }
 
             Row() {
                 Button(
                     modifier = Modifier.padding(0.dp, 0.dp, 20.dp, 0.dp),
                     onClick = {
+
+                        //h ja k pitää ottaa vastakkaiset arvot!!
                         k = kText.toFloat()
                         h = hText.toFloat()
                         r = rText.toFloat()
+
+                        //Center dot
+                        Calculator4lineChartListCenter.add(Point(h+0.05f, k))
+                        Calculator4lineChartListCenter.add(Point(h, k-0.05f))
+                        Calculator4lineChartListCenter.add(Point(h-0.05f, k))
+                        Calculator4lineChartListCenter.add(Point(h, k-0.05f))
+                        Calculator4lineChartListCenter.add(Point(h+0.05f, k))
 
                         CoroutineScope(IO).launch {
                             while(t <= 2){
@@ -206,25 +311,19 @@ fun GraphingCalculatorScreen4(navController: NavController) {
 
                                 yValue = e2.calculate().toFloat()
 
-                                Log.d("CircleTest", "h = $h")
-                                Log.d("CircleTest", "k = $k")
-                                Log.d("CircleTest", "r = $r")
-                                Log.d("CircleTest", "t = $t")
-                                Log.d("CircleTest", "x value: $xValue")
-                                Log.d("CircleTest", "y value: $yValue")
-                                Log.d("CircleTest", "-----")
+                                Log.d("CircleTest", "Point added: ($xValue, $yValue)")
 
                                 Calculator4lineChartList.add(Point(xValue, yValue))
 
                                 t = t + 0.025f
                             }
 
-                            kText = " "
-                            kText = ""
+                            uiUpdate = " "
+                            uiUpdate = ""
                         }
                     }
                 ) {
-                    Text("Piirrä kaavio")
+                    Text("Piirrä ympyrä")
                 }
 
                 Button(
@@ -232,7 +331,7 @@ fun GraphingCalculatorScreen4(navController: NavController) {
 
                     }
                 ) {
-                    Text("Tyhjennä taulukko")
+                    Text("Tyhjennä kuvaaja")
                 }
             }
 
@@ -253,6 +352,8 @@ fun GraphingCalculatorScreen4(navController: NavController) {
                     Text("Takaisin päävalikkoon")
                 }
             }
+
+            Text(text = uiUpdate)
         }
     }
 }
